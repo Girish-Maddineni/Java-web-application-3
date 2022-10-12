06136c74ffcb2aec79fbd1153e279091a819218a
Girishkumar <89227136+MaddineniGirish@users.noreply.github.com>
2022-10-12 09:59:22 -0700

    Add files via upload

---
+++ Terraform/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "var.region"
+}
+
+resource "aws_instance" "ec2" {
+  ami                    = "ami-08e2d37b6a0129927"
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-055b559efa38b65a7"
+  vpc_security_group_ids = ["sg-08456076973730290"]
+  tags = {
+    Name = "demo-ec2"
+  }
+}
